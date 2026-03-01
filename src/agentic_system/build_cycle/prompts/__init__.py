d0ebd06727304a39db42ccebdeb6d5f96e41659a
h19overflow <hamzakhaledlklk@gmail.com>
2026-03-01 21:00:44 +0800

    chore(prompts): remove old monolithic node_planner prompt

--- src/agentic_system/build_cycle/prompts/__init__.py
+++ src/agentic_system/build_cycle/prompts/__init__.py
@@ -1,9 +1,7 @@
-from src.agentic_system.build_cycle.prompts.node_planner import NODE_PLANNER_SYSTEM_PROMPT
 from src.agentic_system.build_cycle.prompts.node_worker import NODE_WORKER_SYSTEM_PROMPT
 from src.agentic_system.build_cycle.prompts.debugger import DEBUGGER_SYSTEM_PROMPT
 
 __all__ = [
-    "NODE_PLANNER_SYSTEM_PROMPT",
     "NODE_WORKER_SYSTEM_PROMPT",
     "DEBUGGER_SYSTEM_PROMPT",
 ]
